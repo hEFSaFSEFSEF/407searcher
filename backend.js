594233db4aae3ab04e289bbfd2cad33f4a33dc44
hEFSaFSEFSEF <hqkk257@gmail.com>
2026-08-08 04:49:05 +0200

    Update backend.js

--- backend.js
+++ backend.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static('.'));
+app.get('/', (req, res) => {
+    res.send('✅ Render fonctionne !');
+});
 
 app.listen(PORT, () => {
     console.log(`✅ Serveur démarré sur le port ${PORT}`);
